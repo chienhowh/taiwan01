ee97b7f4151ffc9a53f5e2240c6bc61d1c2e9c24
chienhowh <63964025+chienhowh@users.noreply.github.com>
2020-08-24 18:25:22 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { Project01Module } from './project-01/project-01.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Project01Module
   ],
   providers: [],
   bootstrap: [AppComponent]
